47af50a81107cac2e4ec244f74d50c24027c1d5b
David Fischer <davidvfischer@gmail.com>
2024-01-08 22:33:43 +0100

    kinda works

--- packages/lib/nodes/widgets/TextWidget.ts
+++ packages/lib/nodes/widgets/TextWidget.ts
@@ -1,3 +1,4 @@
+/* eslint-disable immutable/no-mutation */
 import { IWidget, widgetTypes } from 'litegraph.js'
 
 import { LGraphNode } from '../litegraph-extensions'
@@ -29,6 +30,80 @@ export class TextWidget implements IWidget {
     const x = 0
     let y = posY
     ctx.save()
+    const canvas = ctx.canvas
+    const canvasRect = canvas.getBoundingClientRect()
+    // get this node's position relative to the canvas
+    const nodeRect = node.getBounding()
+
+    // get canvas transform info
+    const transform = ctx.getTransform()
+
+    // get the canvas scale factor
+    const scale = transform.a
+
+    // get the canvas offset
+    const left = canvasRect.left + window.pageXOffset
+
+    const top = canvasRect.top + window.pageYOffset
+
+    // get the node's position relative to the canvas
+    const nodeX = nodeRect[0] + left
+    const nodeY = nodeRect[1] + top
+
+    const nodeWidth = nodeRect[2] * scale
+    const nodeHeight = nodeRect[3] * scale
+
+    // offset based on canvas scale and transform
+    const nodeLeft = nodeX * scale + transform.e
+    const nodeTop = nodeY * scale + transform.f
+
+    // insert html text field above canvas if it doesn't exist
+    if (!document.getElementById('textWidget' + node.id)) {
+      const html = document.createElement('input') as HTMLInputElement
+      // give it a unique identifier
+      html.id = 'textWidget' + node.id
+      // placeholder text
+      html.placeholder = 'Enter text'
+
+      // label input form field
+      html.name = 'textWidget' + node.id
+      // set the value
+      html.value = node.properties.value
+
+      html.type = 'text'
+      html.style.position = 'absolute'
+      html.style.left = `${nodeLeft}px`
+      html.style.top = `${nodeTop}px`
+      html.style.width = `${width}px`
+      html.style.height = `${height}px`
+      html.style.border = 'none'
+      html.style.padding = '0px'
+      html.style.margin = '0px'
+      html.style.outline = 'none'
+      html.style.zIndex = '30'
+      html.style.textAlign = 'left'
+      html.style.font = '14px Arial'
+      html.style.color = 'white'
+      html.style.backgroundColor = 'red'
+      html.style.opacity = '0.75'
+      html.style.overflow = 'hidden'
+      html.style.resize = 'none'
+      html.onchange = () => {
+        node.properties.value = html.value
+      }
+      document.body.appendChild(html)
+    } else {
+      // if it does exist, update the value
+      const html = document.getElementById('textWidget' + node.id) as HTMLInputElement
+      html.style.position = 'absolute'
+      html.style.left = `${nodeLeft}px`
+      html.style.top = `${nodeTop}px`
+      html.style.width = `${width}px`
+      html.style.height = `${height}px`
+      if (html) {
+        html.value = node.properties.value
+      }
+    }
 
     // draw rect behind text
     ctx.fillStyle = '#222'
