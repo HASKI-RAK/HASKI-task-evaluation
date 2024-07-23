9870b4bfaf33aedc8e0a8a36eefb9051462f7d64
David Fischer <davidvfischer@gmail.com>
2024-07-23 17:41:16 +0200

    feat: Add LTI dynamic registration endpoint and component

--- packages/frontend/src/pages/App.tsx
+++ packages/frontend/src/pages/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable simple-import-sort/imports */
 import {
   createTheme,
   CssBaseline,
@@ -5,63 +6,19 @@ import {
   ThemeProvider,
   useMediaQuery
 } from '@mui/material'
-import { createContext, useEffect, useMemo, useState } from 'react'
+import { createContext, useMemo, useState } from 'react'
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  RouterProvider,
-  useSearchParams
+  RouterProvider
 } from 'react-router-dom'
 
 import ErrorBoundary from '@/components/ErrorBoundary'
 import Editor from '@/pages/Editor'
+import { LtiRegister } from './lti/LtiRegister'
 
 const ColorModeContext = createContext({ toggleColorMode: () => {} })
-const LtiRegister = () => {
-  const [searchParams] = useSearchParams()
-  useEffect(() => {
-    // check if searchparams has openid_configuration and registration_token
-    // if so, send a request to the registration endpoint,
-    // and then send a post message to the parent window
-    if (
-      searchParams.get('openid_configuration') &&
-      searchParams.get('registration_token')
-    ) {
-      // send request to registration endpoint
-      fetch(
-        'http://localhost:5000/v1/lti/register?' + searchParams.toString(),
-
-        {
-          method: 'GET'
-        }
-      )
-        // send post message to parent window
-        // (window.opener || window.parent).postMessage({subject:'org.imsglobal.lti.close'}, '*');
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error('Could not register tool: ' + response.statusText)
-          }
-          return response.json()
-        })
-        .then((data) => {
-          console.log(data)
-        })
-        .finally(() => {
-          ;(window.opener || window.parent).postMessage(
-            { subject: 'org.imsglobal.lti.close' },
-            '*'
-          )
-        })
-    }
-  })
-
-  return (
-    <div>
-      <h1>Please wait while we register your tool...</h1>
-    </div>
-  )
-}
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
---
+++ packages/frontend/src/pages/lti/LtiRegister.tsx
@@ -0,0 +1,47 @@
+import { useEffect } from 'react'
+import { useSearchParams } from 'react-router-dom'
+
+export const LtiRegister = () => {
+  const [searchParams] = useSearchParams()
+  useEffect(() => {
+    // check if searchparams has openid_configuration and registration_token
+    // if so, send a request to the registration endpoint,
+    // and then send a post message to the parent window
+    if (
+      searchParams.get('openid_configuration') &&
+      searchParams.get('registration_token')
+    ) {
+      // send request to registration endpoint
+      fetch(
+        'http://localhost:5000/v1/lti/register?' + searchParams.toString(),
+
+        {
+          method: 'GET'
+        }
+      )
+        // send post message to parent window
+        // (window.opener || window.parent).postMessage({subject:'org.imsglobal.lti.close'}, '*');
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Could not register tool: ' + response.statusText)
+          }
+          return response.json()
+        })
+        .then((data) => {
+          console.log(data)
+        })
+        .finally(() => {
+          ;(window.opener || window.parent).postMessage(
+            { subject: 'org.imsglobal.lti.close' },
+            '*'
+          )
+        })
+    }
+  })
+
+  return (
+    <div>
+      <h1>Please wait while we register your tool...</h1>
+    </div>
+  )
+}
